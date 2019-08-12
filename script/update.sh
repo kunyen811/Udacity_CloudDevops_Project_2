f920f666ed777e3d2e0473f4a6d86553061f2840
Ken (Kun-Yen) Tsai / FPC Management Center <kytsai@fpcusa.com>
2019-08-12 11:04:17 -0400

    modified

--- script/update.sh
+++ script/update.sh
@@ -1,6 +1,7 @@
-aws cloudformation create-stack \
+aws cloudformation update-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --parameters file://$3 \
+--capabilities CAPABILITY_IAM \
 --region=us-west-2 \
 --profile udacityDevops
